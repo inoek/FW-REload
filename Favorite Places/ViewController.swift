2c1fa5e5f060707123d566418f555068f879e404
iNoek <noekihar@gmail.com>
2020-01-23 14:06:14 +0300

    Initial Commit

---
+++ Favorite Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Favorite Places
+//
+//  Created by Игорь Ноек on 23.01.2020.
+//  Copyright © 2020 Игорь Ноек. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
